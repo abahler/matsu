d9b1ac0ad9918f03278edb59179ce2a1879fd204
Alex Bahler <alexbahler@gmail.com>
2014-01-31 09:48:32 -0800

    Updated production settings to reflect new MEDIA_* variables

--- matsu/matsu/settings_prod.py
+++ matsu/matsu/settings_prod.py
@@ -24,7 +24,10 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = [u'mastu-test.opensciencedatacloud.org', u'www.matsu-test.opensciencedatacloud.org']
+ALLOWED_HOSTS = [u'www.matsu-test.opensciencedatacloud.org',
+  u'matsu-test.opensciencedatacloud.org',
+  u'www.matsu.opensciencedatacloud.org',
+  u'matsu.opensciencedatacloud.org']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -51,12 +54,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/home/ubuntu/matsu/matsu/assets/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -72,7 +75,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows. Use absolute paths, not relative paths.
-    ("assets", "/home/ubuntu/matsu/matsu/static"),
+    ("assets", "/home/ubuntu/matsu/matsu/static/"),
 )
 
 # List of finder classes that know how to find static files in
